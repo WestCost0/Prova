b1461eff09156b8924528a0dbf8a98890ea73d84
Francesco Fattori <Francesco@iMac-di-Francesco.local>
2013-10-10 19:57:44 +0200

    Initial Commit

---
+++ Prova/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Prova
+//
+//  Created by Francesco Fattori on 10/10/13.
+//  Copyright (c) 2013 Francesco Fattori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Prova/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prova
+//
+//  Created by Francesco Fattori on 10/10/13.
+//  Copyright (c) 2013 Francesco Fattori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
